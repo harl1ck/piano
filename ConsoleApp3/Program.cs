be780f272c4b74db378cc35fed4b805d487ee309
harl1ck <anastasia.ovtsunova@gmail.com>
2023-10-09 15:16:58 +0300

    Наговнякала

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,95 +1,91 @@
-﻿namespace ConsoleApp3
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
-        static int[] SixthOctave = new int[] { 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976 };
-
-        static int[] SeventhOctave = new int[] { 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951 };
-
-        static int[] EigthOctave = new int[] { 4186, 4435, 4699, 4978, 5274, 5588, 5920, 6272, 6645, 7040, 7459, 7902 };
-
-        static void PianoOctave(int[] Octave)
-        {
-            while (true)
-            {
-                System.ConsoleKeyInfo key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.F6 || key.Key == ConsoleKey.F7 || key.Key == ConsoleKey.F8)
-                {
-                    break;
-                }
-                switch (key.Key)
-                {
-                    case ConsoleKey.A:
-                        Console.Beep(Octave[0], 300);
-                        break;
-                    case ConsoleKey.W:
-                        Console.Beep(Octave[1], 300);
-                        break;
-                    case ConsoleKey.S:
-                        Console.Beep(Octave[2], 300);
-                        break;
-                    case ConsoleKey.E:
-                        Console.Beep(Octave[3], 300);
-                        break;
-                    case ConsoleKey.D:
-                        Console.Beep(Octave[4], 300);
-                        break;
-                    case ConsoleKey.F:
-                        Console.Beep(Octave[5], 300);
-                        break;
-                    case ConsoleKey.T:
-                        Console.Beep(Octave[6], 300);
-                        break;
-                    case ConsoleKey.G:
-                        Console.Beep(Octave[7], 300);
-                        break;
-                    case ConsoleKey.Y:
-                        Console.Beep(Octave[8], 300);
-                        break;
-                    case ConsoleKey.H:
-                        Console.Beep(Octave[9], 300);
-                        break;
-                    case ConsoleKey.U:
-                        Console.Beep(Octave[10], 300);
-                        break;
-                    case ConsoleKey.J:
-                        Console.Beep(Octave[11], 300);
-                        break;
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
+            ConsoleKeyInfo key;
             Console.WriteLine("Белые клавиши: A, S, D, F, G, H, J; Чёрные клавиши: W, E, T, Y, U.\n" +
                 "Переключение между октавами осуществляется с помощью F6, F7, F8.\n" +
                 "Повторное нажатие на клавишу октавы останавливает её работу.\n" +
                 "Для выхода из программы нажмите на любую клавишу или Enter.");
-
             while (true)
             {
-
-                System.ConsoleKeyInfo Octave = Console.ReadKey(true);
-                switch (Octave.Key)
-                {
-                    case ConsoleKey.F6:
-                        Console.WriteLine("Текущая октава: 6");
-                        PianoOctave(SixthOctave);
-                        continue;
-                    case ConsoleKey.F7:
-                        Console.WriteLine("Текущая октава: 7");
-                        PianoOctave(SeventhOctave);
-                        continue;
-                    case ConsoleKey.F8:
-                        Console.WriteLine("Текущая октава: 8");
-                        PianoOctave(EigthOctave);
-                        continue;
-                    default:
-                        Environment.Exit(0);
-                        break;
-                }
+                key = Console.ReadKey();
+                Piano(key);
             }
         }
-        
+        static int[] Octave = new int[] { };
+        static int[] SixthOctave = new int[] { 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976 };
+        static int[] SeventhOctave = new int[] { 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951 };
+        static int[] EigthOctave = new int[] { 4186, 4435, 4699, 4978, 5274, 5588, 5920, 6272, 6645, 7040, 7459, 7902 };
+        static void beep(int[] O, int N)
+        {
+            Console.Beep(O[N], 300);
+        }
+        static void change(int[] next)
+        {
+            Octave = next;
+        }
+        static void Piano(ConsoleKeyInfo OctaKey)
+        {
+            switch (OctaKey.Key)
+            {
+                case ConsoleKey.F6:
+                    change(SixthOctave);
+                    Console.WriteLine("\nТекущая октава: 6");
+                    break;
+                case ConsoleKey.F7:
+                    change(SeventhOctave);
+                    Console.WriteLine("\nТекущая октава: 7");
+                    break;
+                case ConsoleKey.F8:
+                    change(EigthOctave);
+                    Console.WriteLine("\nТекущая октава: 8");
+                    break;
+                case ConsoleKey.A:
+                    beep(Octave, 0);
+                    break;
+                case ConsoleKey.W:
+                    beep(Octave, 1);
+                    break;
+                case ConsoleKey.S:
+                    beep(Octave, 2);
+                    break;
+                case ConsoleKey.E:
+                    beep(Octave, 3);
+                    break;
+                case ConsoleKey.D:
+                    beep(Octave, 4);
+                    break;
+                case ConsoleKey.F:
+                    beep(Octave, 5);
+                    break;
+                case ConsoleKey.T:
+                    beep(Octave, 6);
+                    break;
+                case ConsoleKey.G:
+                    beep(Octave, 7);
+                    break;
+                case ConsoleKey.Y:
+                    beep(Octave, 8);
+                    break;
+                case ConsoleKey.H:
+                    beep(Octave, 9);
+                    break;
+                case ConsoleKey.U:
+                    beep(Octave, 10);
+                    break;
+                case ConsoleKey.J:
+                    beep(Octave, 11);
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
     }
 }
